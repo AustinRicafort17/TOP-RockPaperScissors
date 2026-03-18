ec7e84d662e632b533d78573647077a2c12b0bd0
Lee Austin Ricafort <leeaustinricafort1998@gmail.com>
2026-03-19 06:52:46 +0900

    done with game logic

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function GetComputerChoice (){
     result = Math.floor(Math.random() * 3 ) + 1 ;
 
@@ -14,23 +12,57 @@ function GetComputerChoice (){
     }
 }
 
-
 function GetHumanChoice (){
     choice = prompt("Enter either 'rock', 'paper' or 'sciccors'")
+    choice = choice.toUpperCase()
     return choice
 }
 
 
+const HumanChoice = GetHumanChoice()
+const ComputerChoice = GetComputerChoice()
+
+
+
+function PlayRound(Human,Computer){
+    if (Human === "ROCK" && Computer === "ROCK"){
+        return "draw"
+    }    
+    else if (Human === "ROCK" && Computer === "PAPER"){
+        return "HumanWin"
+    }
+    else if (Human === "ROCK" && Computer === "SCISSORS" ){
+        return "ComputerWin"
+    }
+    else if (Human === "PAPER" && Computer === "ROCK"){
+        return "HumanWin"
+    }
+    else if (Human === "PAPER" && Computer === "PAPER"){
+        return "draw"
+    }
+    else if (Human === "PAPER" && Computer === "SCISSORS"){
+        return "ComputerWin"
+    }
+    else if (Human === "SCISSORS" && Computer === "ROCK"){
+        return "ComputerWin"
+    }
+    else if (Human === "SCISSORS" && Computer === "PAPER"){
+        return "HumanWin"
+    }
+    else if (Human === "SCISSORS" && Computer === "SCISSORS"){
+        return "draw"
+    }
+}
+
+
+console.log(HumanChoice)
+console.log(ComputerChoice)
+console.log(PlayRound(HumanChoice,ComputerChoice))
+
+
+let draw = 0
 let HumanScore = 0
 let ComputerScore = 0
 
 
-function PlayRound(Human,Computer){
 
-    
-
-
-}
-
-const HumanChoice = GetHumanChoice()
-const ComputerChoice = GetComputerChoice()
