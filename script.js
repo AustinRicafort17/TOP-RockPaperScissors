851be18f2f78b3707c136b054e9b570d15659875
Lee Austin Ricafort <leeaustinricafort1998@gmail.com>
2026-03-20 14:50:59 +0900

    figuring our the scoreboard

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function GetHumanChoice (){
 
 function PlayRound(Human,Computer){
     if (Human === "ROCK" && Computer === "ROCK"){
-        return "draw"
+        return "draw" ; alert("It's a DRAW!")
     }
     else if (Human === "ROCK" && Computer === "PAPER"){
-        return "HumanWin"
+        return "ComputerWin" ; alert("You lose! paper beats rock")
     }
     else if (Human === "ROCK" && Computer === "SCISSORS" ){
-        return "ComputerWin"
+        return "HumanWin" ; alert("You Win! rock bears scissors")
     }
     else if (Human === "PAPER" && Computer === "ROCK"){
         return "HumanWin"
@@ -55,7 +55,7 @@ function PlayRound(Human,Computer){
 
 function Playgame(){
 
-    alert("ROUND 1") ; round1 =  PlayRound(GetHumanChoice(),GetComputerChoice())
+    alert("ROUND 1") ; round1 = PlayRound(GetHumanChoice(),GetComputerChoice())
     alert("ROUND 2") ; round2 = PlayRound(GetHumanChoice(),GetComputerChoice())
     alert("ROUND 3") ; round3 = PlayRound(GetHumanChoice(),GetComputerChoice())
     alert("ROUND 4") ; round4 = PlayRound(GetHumanChoice(),GetComputerChoice())
@@ -77,4 +77,77 @@ let HumanScore = 0
 let ComputerScore = 0
 
 
+function Score1(){
+    if (round1 === "ComputerWin"){
+        ComputerScore = ComputerScore + 1
+    }
+    else if (round1 === "HumanWin"){
+        HumanScore = HumanScore + 1
+    }
+    else if (round1 === "draw"){
+        draw = draw + 1
+    }
+}
+function Score2(){
+    if (round2 === "ComputerWin"){
+        ComputerScore = ComputerScore + 1
+    }
+    else if (round2 === "HumanWin"){
+        HumanScore = HumanScore + 1
+    }
+    else if (round2 === "draw"){
+        draw = draw + 1
+    }
+}
+function Score3(){ 
+        if (round3 === "ComputerWin"){
+        ComputerScore = ComputerScore + 1
+    }
+    else if (round3 === "HumanWin"){
+        HumanScore = HumanScore + 1
+    }
+    else if (round3 === "draw"){
+        draw = draw + 1
+    }
+}
+function Score4(){ 
+        if (round4 === "ComputerWin"){
+        ComputerScore = ComputerScore + 1
+    }
+    else if (round4 === "HumanWin"){
+        HumanScore = HumanScore + 1
+    }
+    else if (round4 === "draw"){
+        draw = draw + 1
+    }
+}
+function Score5(){ 
+    if (round5 === "ComputerWin"){
+        ComputerScore = ComputerScore + 1
+    }
+    else if (round5 === "HumanWin"){
+        HumanScore = HumanScore + 1
+    }
+    else if (round5 === "draw"){
+        draw = draw + 1
+    }
+}
+
+Score1()
+Score2()
+Score3()
+Score4()
+Score5()
+
+
+console.log(draw)
+console.log(HumanScore)
+console.log(ComputerScore)
+
+
+function Scoreboard(){
+    if(HumanScore > ComputerScore){
+        alert(HumanScore + "You Win!")
+    }
+}
 
